e641f861b332f96df586163f1a704b7551ec2b70
BTavkhelidze <beqatavkhelidze4@gmail.com>
2024-07-22 14:34:48 +0400

    feature html
    adding footerResponsive/language.html

--- js/languageChange.js
+++ js/languageChange.js
@@ -1,29 +1,43 @@
-const desktop__lang__geo = document.querySelector('.desktop__lang_geo_text');
-const desktop__lang_eng_text = document.querySelector(
-  '.desktop__lang_eng__text'
+const desktop__lang__geo = Array.from(
+  document.querySelectorAll('.desktop__lang_geo_text')
+);
+const desktop__lang_eng_text = Array.from(
+  document.querySelectorAll('.desktop__lang_eng__text')
 );
 
-const desktop__lang = document.querySelector('.desktop__lang'); // main lang icon
-const desktop__lang_eng = document.querySelector('.desktop__lang_eng'); // second option lang icon
+console.log(desktop__lang__geo);
+console.log(desktop__lang_eng_text);
+const desktop__lang = Array.from(document.querySelectorAll('.desktop__lang')); // main lang icon
+const desktop__lang_eng = Array.from(
+  document.querySelectorAll('.desktop__lang_eng')
+); // second option lang icon
+
+console.log(desktop__lang);
+console.log(desktop__lang_eng);
 
 let currentLanguage = -1;
 
 const languageToggle = () => {
   if (currentLanguage === -1) {
-    desktop__lang__geo.textContent = 'Eng';
-    desktop__lang_eng_text.textContent = 'ქარ';
+    desktop__lang__geo.forEach((leng) => (leng.textContent = 'Eng'));
+    desktop__lang_eng_text.forEach((leng) => (leng.textContent = 'ქარ'));
     currentLanguage = 1; // Update currentLanguage
   } else {
-    desktop__lang_eng_text.textContent = 'Eng';
-    desktop__lang__geo.textContent = 'ქარ';
+    desktop__lang_eng_text.forEach((leng) => (leng.textContent = 'Eng'));
+    desktop__lang__geo.forEach((leng) => (leng.textContent = 'ქარ'));
     currentLanguage = -1; // Update currentLanguage
   }
+  // Hide the secondary language options after toggling
 };
 
-desktop__lang.addEventListener('click', () => {
-  languageToggle();
-});
+// desktop__lang.forEach((btn) => {
+//   btn.addEventListener('click', () => {
+//     languageToggle();
+//   });
+// });
 
-desktop__lang_eng.addEventListener('click', () => {
-  languageToggle();
+desktop__lang_eng.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    languageToggle();
+  });
 });
