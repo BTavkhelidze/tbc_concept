2571d168b4272e76fad40c099259327faf67d8c2
BTavkhelidze <beqatavkhelidze4@gmail.com>
2024-07-23 01:31:24 +0400

    feature html
    correcting footer burger menu

--- js/footer.js
+++ js/footer.js
@@ -10,16 +10,28 @@ const icon = document.querySelector('.closed');
 // });
 
 let currentIcon = './assets/svg/burger_menu_arrow_down.svg';
+let openMenuIndex = -1;
 
 for (let i = 0; i < br_menu_title.length; i++) {
   br_menu_title[i].addEventListener('click', () => {
+    // Close the previously opened menu if it exists and is different from the current one
+    if (openMenuIndex !== -1 && openMenuIndex !== i) {
+      hidden_menu[openMenuIndex].classList.add('hidden');
+    }
+
+    // Toggle the current menu
     hidden_menu[i].classList.toggle('hidden');
 
-    if (currentIcon === './assets/svg/burger_menu_arrow_down.svg') {
-      currentIcon = './assets/svg/burger_menu_arrow_up.svg';
-    } else {
+    // Update the openMenuIndex
+    if (hidden_menu[i].classList.contains('hidden')) {
+      openMenuIndex = -1;
       currentIcon = './assets/svg/burger_menu_arrow_down.svg';
+    } else {
+      openMenuIndex = i;
+      currentIcon = './assets/svg/burger_menu_arrow_up.svg';
     }
+
+    // Update the icon
     icon.src = currentIcon;
   });
 }
